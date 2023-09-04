ca68256c9394b64fdcf8baeddb6b7abab0ad40ea
Chris Deck <deckchris95@gmail.com>
2023-09-04 10:56:35 -0400

    undid commit

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import requests
+from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/healthz', methods=['GET'])
+# def index():
+#     print('health page reached')
+#     return '200 OK'
+
+# if __name__ == '__main__':
+#     print('Starting Flask app...')
+#     app.run()
 
 # Credentials
 load_dotenv()
@@ -19,10 +31,49 @@ async def on_ready():
     print('Bot ID: {}'.format(client.user.id))
 
 # Command
-@client.command(name='mybag', help='Retreives the details of your current main disc golf bag.')
-async def mybag(ctx):
-    print(ctx.message.author.id)
-    await ctx.send(f'Bags are not yet configured. Please check back later.')   
+@client.command(name='subjectinfo', help='Retreives the image for the specified token.')
+async def getimage(ctx, tokenID):
+    jsonPath = 'https://apecountryclub.s3.amazonaws.com/JSON/' + tokenID + '.json'
+    gifpath = 'https://apecountryclub.s3.amazonaws.com/IMAGE/' + tokenID + '.gif'
+    path = 'https://apecountryclub.s3.amazonaws.com/IMAGE/' + tokenID + '.png'
+
+    jsonR = requests.get(jsonPath)
+    if jsonR.status_code == requests.codes.ok:
+        body = jsonR.json();
+        print(body)
+        name = body['name']
+        if('Tripped' in name):
+            #trippy ape
+            r = requests.head(path)
+            day = body['day']
+            daysLeft = str(15-day)
+            if r.status_code == requests.codes.ok:
+                await ctx.send('Subject #' + tokenID + ' has experienced unfortunate side effects during the trials. These will resolve within ' + daysLeft + ' days.')
+                await ctx.send(path)
+            else:
+                r = requests.head(gifpath)
+                print(gifpath)
+                if r.status_code == requests.codes.ok:
+                    await ctx.send('Subject #' + tokenID + ' has experienced unfortunate side effects during the trials. These will resolve within ' + daysLeft + ' days.')
+                    await ctx.send(gifpath)
+                else:
+                    await ctx.send("Please ensure the tokenID you are entering is correct and try again.")
+        else:
+            #normal ape
+            r = requests.head(path)
+            if r.status_code == requests.codes.ok:
+                await ctx.send('Subject #' + tokenID + ' has undergone trials and came out successfully with limited to no side effects.')
+                await ctx.send(path)
+            else:
+                r = requests.head(gifpath)
+                print(gifpath)
+                if r.status_code == requests.codes.ok:
+                    await ctx.send('Subject #' + tokenID + ' has undergone trials and came out successfully with limited to no side effects.')
+                    await ctx.send(gifpath)
+                else:
+                    await ctx.send("Please ensure the tokenID you are entering is correct and try again.")
+    else:
+        await ctx.send("Please ensure the tokenID you are entering is correct and try again.")
     
 # Run the bot
 client.run(TOKEN)
